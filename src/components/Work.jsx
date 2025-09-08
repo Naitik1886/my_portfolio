6c1249dbcacafacf77aa9cdddee248739f43db97
Naitik <mymailofficial1886@gmail.com>
2025-09-08 20:35:50 +0530

    last

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -22,6 +22,7 @@ export default function Work({
       <div className="vide-container hidden sm:flex  sm:w-100 sm:h-120 text-center justify-evenly items-center md:w-128 md:h-120  lg:w-160 lg:h-120   xl:w-240 xl:h-120 ">
         <video
           preload="metadata"
+          poster="preview.jpg"
           className="sm:w-72  sm:h-60 md:w-80 md:h-60 lg:w-96 lg:h-72 xl:w-xl xl:h-96"
           autoPlay
           muted
@@ -53,6 +54,7 @@ export default function Work({
       <div className="w-60 h-180 mt-5 text-center items-center sm:hidden">
         <video
           preload="metadata"
+          poster="mobpreview.jpg"
           className="w-60  h-100 mb-9"
           autoPlay
           muted
